0d63b4c13f0339fd12b27e936d7ed9a6f2dab945
AnirudhTEAM <63502373+ANIRUDHTEAM1000@users.noreply.github.com>
2021-06-22 22:21:03 +0530

    Update settings.py

--- unq_coder/unq_coder/settings.py
+++ unq_coder/unq_coder/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static'),
+    os.path.join(BASE_DIR,'unq_coder/static'),
 ]
 
 STATICFILES_ROOT = os.path.join(BASE_DIR,'asserts')
